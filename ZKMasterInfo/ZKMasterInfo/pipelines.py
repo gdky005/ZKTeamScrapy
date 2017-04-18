f91c59cb7149e3dd39b8797d9dc47020611a5bc6
qingw <qingw@jumei.com>
2017-04-18 11:51:35 +0800

    1. 添加 大神个人信息的爬虫。

---
+++ ZKMasterInfo/ZKMasterInfo/pipelines.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import Constant as Globle
+import pymysql
+import pymysql.cursors
+
+
+class ZkmasterinfoPipeline(object):
+    host = Globle.Constant.host
+    port = Globle.Constant.port
+    user = Globle.Constant.user
+    password = Globle.Constant.password
+    database_name = Globle.Constant.database_name
+    movie_table_name = Globle.Constant.movie_table_name
+    charset = Globle.Constant.charset
+
+    def __init__(self):
+        self.conn = pymysql.connect(host=self.host, port=self.port, user=self.user, passwd=self.password,
+                                    db=self.database_name, charset=self.charset)
+
+    def process_item(self, item, spider):
+        # 给库中插入数据
+        cur = self.conn.cursor()
+
+        name = item['name']
+        uid = item['uid']
+        img = item['img']
+        info = item['info']
+        isVip = item['isVip']
+        index = item['index']
+        blog = item['blog']
+
+        sql = "INSERT INTO " + self.movie_table_name + " (name, uid, img, info, isVip, blog)" \
+                                                       " VALUES (%s, %s, %s, %s, %s, %s)"
+        cur.execute(sql, (name, uid, img, info, isVip, blog))
+        cur.close()
+        self.conn.commit()
+
+        return item
---
+++ ZKMasterInfo/ZKMasterInfo/spiders/MasterInfo.py
@@ -0,0 +1,30 @@
+from scrapy import Selector
+from scrapy.spiders import CrawlSpider
+from ZKMasterInfo.items import ZkmasterinfoItem
+
+
+class MasterInfo(CrawlSpider):
+    name = "MasterInfo"  # 爬虫命名
+    start_urls = ['http://blog.csdn.net/singwhatiwanna?viewmode=contents']  # 要爬取的页面地址
+
+    def parse(self, response):
+        print(response.body.decode('utf-8'))
+
+        item = ZkmasterinfoItem()
+
+        selector = Selector(response)
+
+        name = selector.xpath('//span/a[@class="user_name"]/text()').extract()[0]  # 用户名
+        info = selector.xpath('//h3/text()').extract()[0]  # 用户名
+        user_img = selector.xpath('//div[@id="blog_userface"]/a/img/@src').extract()[0]  # 用户图像
+        blog = response.url  # 当前 url
+
+        item['name'] = name
+        item['uid'] = '10001'
+        item['img'] = user_img
+        item['info'] = info
+        item['isVip'] = '1'
+        item['index'] = '11'
+        item['blog'] = blog
+
+        yield item
